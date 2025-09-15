34d42f2b2c03790c9ba9051654f57c7f0fed891b
Alpha <georgeofori2005@gmail.com>
2025-09-15 12:02:43 +0000

    Sept 15, 2025

--- exercise-9/js/todo-list.js
+++ exercise-9/js/todo-list.js
@@ -29,13 +29,104 @@ class Task{
         tasks.splice(id, 1, myTask);
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
+    
+    static delete(id){
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        if (id < 1 || id > tasks.length){
+            throw new Error("Id not found.");
+        }else{
+            delete tasks[id - 1];
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+    }
 }
 
+//Define home function
 
-let task1 = new Task('Monday', 'Go to school');
-//task1.save()
-//localStorage.removeItem('tasks');
+function home(){
+    console.log('Welcome to Alpha ToDo');
+    console.log('1.Create A Task.');
+    console.log('2.View Tasks.');
+    console.log('3.Delete A Task.');
+    let choice = prompt('Chose: ');
+    if (choice !== null){
+        choice = Number(choice);
+        if (!isNaN(choice)){
+            switch (choice){
+                case 1:
+                createTask();
+                break;
+                case 2:
+                viewTasks();
+                break;
+                case 3:
+                deleteTask();
+                break;
+                default:
+                home();
+            }
+        }else{
+            console.log('Wrong input. Please try again');
+            home();
+        }
+    }else{
+        console.log("Good Bye");
+    }
+}
 
-//console.log(Task.edit(1, "title", "Tuesday"));
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-console.log(tasks[0].title);
+//Define createTask function
+
+function createTask(){
+    console.log('Create A Task');
+    let title = prompt('The title of your task: ');
+    let description = prompt('Description: ');
+    preview({'title':title, 'description':description, 'status':false});
+}
+
+// Define the preview function
+ 
+function preview(task){
+    console.log('Preview Your Task');
+    for (let key in task){
+        console.log(`${key}:${task[key]}`);
+    }
+    let $confirm = confirm('Do you want to save?');
+    if ($confirm){
+        task = new Task(task['title'], task['description'], task['status']);
+        task.save();
+    }else{
+        home();
+    }
+}
+
+//Create the viewTasks function
+
+function viewTasks(){
+    console.log('View Tasks');
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let i = 1;
+    for (let data of tasks){
+        console.log(`Task ${i}`);
+        i ++;
+        for (let key in data){
+            console.log(`${key}:${data[key]}`);
+        }
+    }
+    let taskId = prompt("Chose a task: ");
+    console.log(`1.Edit`);
+    console.log(`2.Mark as Done`);
+    console.log(`3.Unmark As Done.`);
+    cosole.log('4.Go back');
+    let choice = prompt('Chose: ', 0);
+    switch (choice){
+        case 0:
+        home();
+        break;
+        case 1:
+        editTask(taskId);
+        break;
+    }
+}
+
+home();
+
